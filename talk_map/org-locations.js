8fd401bd68c2fa9ac3064b97ab1415eb7641b14b
staeiou <sgeiger@gmail.com>
2016-08-19 21:38:34 +0000

    map labels link to talks

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,237 +1,242 @@
 var addressPoints = [
   [
-    "17ba0791499db908433b80f37c5fbc89b870084b",
-    33.6170092,
-    -117.92944
-  ],
-  [
-    "356a192b7913b04c54574d18c28d46e6395428ab",
-    30.2711286,
-    -97.7436994
-  ],
-  [
-    "af3e133428b9e25c55bc59fe534248e6a0c0f17b",
-    38.8903961,
-    -77.0841584
-  ],
-  [
-    "0286dd552c9bea9a69ecb3759e7b94777635514b",
-    40.7305991,
-    -73.9865811
-  ],
-  [
-    "fe2ef495a1152561572949784c16bf23abb28057",
-    31.1990035,
-    29.8943785
-  ],
-  [
-    "92cfceb39d57d914ed8b14d0e37643de0797ae56",
-    40.7305991,
-    -73.9865811
-  ],
-  [
-    "fc074d501302eb2b93e2554793fcaf50b3bf7291",
-    21.9811111,
-    -159.371111
-  ],
-  [
-    "d435a6cdd786300dff204ee7c2ef942d3e9034e2",
-    29.4246002,
-    -98.4951404
-  ],
-  [
-    "972a67c48192728a34979d9a35164c1295401b71",
-    33.7774658,
-    -118.188487
-  ],
-  [
-    "12c6fc06c99a462375eeb3f43dfd832b08ca9e17",
-    40.7305991,
-    -73.9865811
-  ],
-  [
-    "91032ad7bbcb6cf72875e8e8207dcfba80173f7c",
-    32.7174209,
-    -117.1627713
-  ],
-  [
-    "22d200f8670dbdb3e253a90eee5098477c95c23d",
-    30.2711286,
-    -97.7436994
-  ],
-  [
-    "9e6a55b6b4563e652a23be9d623ca5055c356940",
-    38.545379,
-    -121.7445834
-  ],
-  [
-    "b6589fc6ab0dc82cf12099d1c2d40ab994e8410c",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "bd307a3ec329e10a2cff8fb87480823da114f8f4",
-    40.4416941,
-    -79.990086
-  ],
-  [
-    "f1f836cb4ea6efb2a0b1b99f41ad8b103eff4b59",
-    37.3855745,
-    -122.0820499
-  ],
-  [
-    "0716d9708d321ffb6a00818614779e779925365c",
-    47.6038321,
-    -122.3300623
-  ],
-  [
-    "7b52009b64fd0a2a49e6d8a939753077792b0554",
-    42.3750997,
-    -71.1056156
-  ],
-  [
-    "f6e1126cedebf23e1463aee73f9df08783640400",
-    48.3059078,
-    14.286198
-  ],
-  [
-    "761f22b2c1593d0bb87e0b606f990ba4974706de",
+    "2009-10-27, Conference proceedings talk at International Symposium on Wikis and Open Collaboration<br/>\n<a href='2009-10-27-wikisym-social-roles-bots'>The Social Roles of Bots and Assisted Editing Tools</a>",
     28.5421175,
     -81.3790461
   ],
   [
-    "b6692ea5df920cad691c20319a6fffd7a4a766b8",
-    41.5051613,
-    -81.6934445
-  ],
-  [
-    "472b07b9fcf2c2451e8781e944bf5f77cd8457c8",
-    22.2793278,
-    114.1628131
-  ],
-  [
-    "98fbc42faedc02492397cb5962ea3a3ffc0a9243",
-    42.3750997,
-    -71.1056156
-  ],
-  [
-    "b3f0c7f6bb763af1be91d9e74eabfeb199dc1f1f",
-    39.7391536,
-    -104.9847033
-  ],
-  [
-    "ac3478d69a3c81fa62e60f5c3696165a4e5e6ac4",
-    33.6251241,
-    130.6180016
-  ],
-  [
-    "0a57cb53ba59c46fc4b692527a38a87c78d84028",
-    53.3497645,
-    -6.2602731
-  ],
-  [
-    "cb7a1d775e800fd1ee4049f7dca9e041eb9ba083",
-    52.374436,
-    4.8979956033677
-  ],
-  [
-    "0ade7c2cf97f75d009975f4d720d1fa6c19f4897",
-    49.2608944,
-    -123.1139382
-  ],
-  [
-    "77de68daecd823babbb58edb1c8e14d7106e83bb",
-    33.6251241,
-    130.6180016
-  ],
-  [
-    "4d134bc072212ace2df385dae143139da74ec0ef",
-    29.4246002,
-    -98.4951404
-  ],
-  [
-    "632667547e7cd3e0466547863e1207a8c0c0c549",
-    35.0185804,
-    135.763835
-  ],
-  [
-    "b1d5781111d84f7b3fe45a0852e59758cd7a87e5",
-    49.2608944,
-    -123.1139382
-  ],
-  [
-    "ca3512f4dfa95a03169c5a670a4c91a19b3077b4",
+    "2010-01-10, Talk at Critical Point of View: Wikipedia and the Politics of Open Knowledge<br/>\n<a href='2010-01-10-cpov-wisdom-of-bots'>The Wisdom of Bots: A Critique of \u2018Self-Organization\u2019 in Wikipedia</a>",
     12.9791198,
     77.5912997
   ],
   [
-    "902ba3cda1883801594b6e1b452790cc53948fda",
-    33.4485866,
-    -112.0773455
-  ],
-  [
-    "827bfc458708f0b442009c9c9836f7e4b65557fb",
-    30.6253463,
-    -96.3271537
-  ],
-  [
-    "c1dfd96eea8cc2b62785275bca38ac261256e278",
-    39.7391536,
-    -104.9847033
-  ],
-  [
-    "f1abd670358e036c31296e66b3b66c382ac00812",
-    -34.6143904,
-    -58.4460878
-  ],
-  [
-    "cb4e5208b4cd87268b208e49452ed6e89a68e0b8",
-    41.5051613,
-    -81.6934445
-  ],
-  [
-    "da4b9237bacccdf19c0760cab7aec4a8359010b0",
-    33.6251241,
-    130.6180016
-  ],
-  [
-    "1b6453892473a467d07372d45eb05abc2031647a",
-    33.6251241,
-    130.6180016
-  ],
-  [
-    "7719a1c782a1ba91c031a682a0a2f8658209adbf",
-    30.2711286,
-    -97.7436994
-  ],
-  [
-    "fe5dbbcea5ce7e2988b8c69bcfdfde8904aabc1f",
+    "2015-05-25, Talk at Annual Meeting of the International Communication Association (ICA)<br/>\n<a href='2015-05-25-ica-wiki-history'>But it Wouldn\u2019t Be an Encyclopedia; It Would Be a Wiki: Wikipedia and the Repurposing of WikiWikiWeb</a>",
     18.38423905,
     -66.0534398736473
   ],
   [
-    "887309d048beef83ad3eabf2a79a64a389ab1c9f",
-    55.6867243,
-    12.5700724
+    "2012-03-31, Talk at GCOE International Symposium on Informatics Education<br/>\n<a href='2012-03-31-gcoe-wikipedia-notifications'>Improving Wikipedia\u2019s Notifications to Rejected Contributors</a>",
+    35.0185804,
+    135.763835
   ],
   [
-    "fa35e192121eabf3dabf9f5ea6abdbcbc107ac3b",
+    "2013-11-25, Talk at Bangkok Scientifique<br/>\n<a href='2013-11-25-bkk-data'>Size Matters: How Big Data Changes Everything</a>",
+    13.7538929,
+    100.8160803
+  ],
+  [
+    "2015-03-24, Workshop presentation at ISchools Conference<br/>\n<a href='2015-03-24-iconf-trace-ethno'>Trace Ethnography Workshop</a>",
+    33.6170092,
+    -117.92944
+  ],
+  [
+    "2014-10-21, Talk at Annual Meeting of the Association of Internet Researchers (AoIR)<br/>\n<a href='2014-10-21-aoir-successor-systems'>Successor Systems: The Role of Reflexive Algorithms in Enacting Ideological Critique</a>",
     35.8404121,
     128.5586643
   ],
   [
-    "5b384ce32d8cdef02bc3a139d4cac0a22bb029e8",
+    "2011-03-04, Talk at Digital Media and Learning (DML)<br/>\n<a href='2011-03-04-dml-bots-governance'>Machine-Generated Content: Bots and the Governance of Wikipedia</a>",
+    33.7774658,
+    -118.188487
+  ],
+  [
+    "2009-09-25, Talk at  the Second Annual Media Sociology Forum<br/>\n<a href='2009-09-25-nyu-trace-ethnography'>Trace Ethnography: An ANT Method for the Study of Sociotechnical Networks</a>",
+    40.7305991,
+    -73.9865811
+  ],
+  [
+    "2015-11-12, Talk at Annual Meeting of the Society for the Social Study of Science (4S)<br/>\n<a href='2015-11-12-4s-bot-multiple'>The Bot Multiple: Unpacking the Materialities of Automated Software Agents</a>",
+    39.7391536,
+    -104.9847033
+  ],
+  [
+    "2008-03-01, Talk at Exploring New Media Worlds<br/>\n<a href='2008-03-01-tamu-communicative-ethnography'>A Communicative Ethnography of Argumentative Strategies in a Wikipedian Content Dispute</a>",
+    30.6253463,
+    -96.3271537
+  ],
+  [
+    "2010-02-25, Conference proceedings talk at Conference on Computer Supported Cooperative Work<br/>\n<a href='2010-02-25-cscw-banning-vandal'>The Work of Sustaining Order in Wikipedia: The Banning of a Vandal</a>",
     32.0835407,
     -81.0998341
   ],
   [
-    "bc33ea4e26e5e1af1408321416956113a4658763",
+    "2010-03-26, Talk at Critical Point of View: Wikipedia and the Politics of Open Knowledge<br/>\n<a href='2010-03-26-cpov-bot-politics'>Bot Politics: How is Automation Changing the Wikipedian Society?  Critical Point of View II</a>",
+    52.374436,
+    4.8979956033677
+  ],
+  [
+    "2014-05-16, Talk at The Contours of Algorithmic Life<br/>\n<a href='2014-05-16-algolife-successor-systems'>Successor Systems: The Role of Reflexive Algorithms in Enacting Ideological Critique</a>",
+    38.545379,
+    -121.7445834
+  ],
+  [
+    "2012-10-12, Talk at Infosocial<br/>\n<a href='2012-10-12-infosocial-trace-literacy'>Trace literacy: a framework for holistically conceptualizing newcomer socialization in socio-technical systems</a>",
     42.0447388,
     -87.6930458
   ],
   [
-    "1574bddb75c78a6fd2251d61e2993b5146201319",
+    "2014-05-23, Panelist at Annual Meeting of the International Communication Association (ICA)<br/>\n<a href='2014-05-23-ica-data-driven-data'>Data\u00ad-Driven Data Research Using Data and Databases: A Practical Critique of Methods and Approaches in \u201cBig Data\u201d Studies</a>",
     47.6038321,
     -122.3300623
+  ],
+  [
+    "2013-02-26, Panelist at ACM Conference on Computer-Supported Cooperative Work (CSCW)<br/>\n<a href='2013-02-26-cscw-community-impact'>Community, Impact, and Credit: Where Do I Submit My Papers?</a>",
+    29.4246002,
+    -98.4951404
+  ],
+  [
+    "2014-05-24, Panelist at Annual Meeting of the International Communication Association (ICA)<br/>\n<a href='2014-05-24-ica-big-data-bullshit'>Big Data is Bullshit&apos;: Scoping the Next 5 Years of Digital Data Research</a>",
+    47.6038321,
+    -122.3300623
+  ],
+  [
+    "2016-06-08, Talk at Algorithms, Automation, and Politics workshop<br/>\n<a href='2016-06-08-ica-algorithms-gatekeeping'>Algorithms as agents of gatekeeping, governance, and articulation work in Wikipedia</a>",
+    33.6251241,
+    130.6180016
+  ],
+  [
+    "2008-07-19, Talk at Annual Wikimedia Conference (Wikimania)<br/>\n<a href='2008-07-19-wikimania-academics-wikipedia'>Conceptions and Misconceptions Academics Hold About Wikipedia</a>",
+    31.1990035,
+    29.8943785
+  ],
+  [
+    "2016-08-13, Talk at PyData SF<br/>\n<a href='2016-08-13-pydata-community-sustainability'>Community Sustainability in Wikipedia: A Review of Research and Initiatives</a>",
+    37.7792808,
+    -122.4192362
+  ],
+  [
+    "2012-10-17, Talk at Annual Meeting of the Society for the Social Study of Science (4S)<br/>\n<a href='2012-10-17-4s-time-to-degree'>Time to Degree: Examining the Experiences of Graduate Students in the Long-Term Ecological Research Network</a>",
+    55.6867243,
+    12.5700724
+  ],
+  [
+    "2014-08-23, Talk at Annual Meeting of the Society for the Social Study of Science (4S)<br/>\n<a href='2014-08-23-4s-successor-systems'>Successor Systems: The Role of Reflexive Algorithms in Enacting Ideological Critique</a>",
+    -34.6143904,
+    -58.4460878
+  ],
+  [
+    "2014-12-09, Talk at Berkman Center for Internet and Society<br/>\n<a href='2014-12-09-berkman-successor-systems'>Supporting Change from Outside Systems with Design and Data</a>",
+    42.3750997,
+    -71.1056156
+  ],
+  [
+    "2011-11-02, Talk at Annual Meeting of the Society for the Social Study of Science (4S)<br/>\n<a href='2011-11-02-4s-internet-is-here'>\u2019The Internet is Here\u2019: The Virtuality of \u2018On-line Communities in Physical Spaces</a>",
+    41.5051613,
+    -81.6934445
+  ],
+  [
+    "2015-03-15, Workshop presentation at CSCW Workshop on Ethics for Studying Sociotechnical Systems in a Big Data World<br/>\n<a href='2015-03-15-cscw-ethics-workshop'>Does Facebook Have Civil Servants? On Governmentality and Computational Social Science</a>",
+    49.2608944,
+    -123.1139382
+  ],
+  [
+    "2012-06-05, Conference proceedings talk at International Conference on Weblogs and Social Media (ICWSM)<br/>\n<a href='2012-06-05-icwsm-socialization-wikipedia'>Defense Mechanism or Socialization Tactic? Improving Wikipedia\u2019s Notifications to Rejected Contributors</a>",
+    53.3497645,
+    -6.2602731
+  ],
+  [
+    "2015-03-15, Workshop presentation at CSCW Workshop on Feminism and Feminist Approaches in Social Computing<br/>\n<a href='2015-03-15-cscw-feminism-workshop'>Situated knowledges and successor systems: developing CSCW systems to enact ideological critiques</a>",
+    49.2608944,
+    -123.1139382
+  ],
+  [
+    "2015-10-23, Talk at Annual Meeting of the Association of Internet Researchers (AoIR)<br/>\n<a href='2015-10-23-aoir-blockbots'>Bot-Based Collective Blocklists in Twitter: The Counterpublic Moderation of a Privately-Owned Networked Public Space</a>",
+    33.4485866,
+    -112.0773455
+  ],
+  [
+    "2009-07-26, Talk at First Annual Wikiconference NYC<br/>\n<a href='2009-07-26-wikiconf-nyc-algorithmc-governance'>Algorithmic Governance: The Social Roles of Bots and Assisted Editing Tools</a>",
+    40.7305991,
+    -73.9865811
+  ],
+  [
+    "2013-08-03, Conference proceedings talk at International Symposium on Wikis and Open Collaboration (WikiSym 2012)<br/>\n<a href='2013-08-03-wikisym-levee-breaks-bots'>When the Levee Breaks: Without Bots, What Happens to Wikipedia\u2019s Quality Control Processes? (with Aaron Halfaker)</a>",
+    22.2793278,
+    114.1628131
+  ],
+  [
+    "2009-10-28, Talk at Annual Meeting of the Society for the Social Study of Science (4S)<br/>\n<a href='2009-10-28-4s-missing-wikipedians'>Where Are the Missing Wikipedians? The Sociology of a Bot</a>",
+    38.8903961,
+    -77.0841584
+  ],
+  [
+    "2012-05-02, Conference proceedings talk at Conference on Human Factors in Computing (CHI)<br/>\n<a href='2012-05-02-altchi-ipoxp'>Black-boxing the user: internet protocol over xylophone players (IPoXP)</a>",
+    30.2711286,
+    -97.7436994
+  ],
+  [
+    "2016-06-11, Panelist at Annual Meeting of the International Communication Association (ICA)<br/>\n<a href='2016-06-11-ica-drowning-in-data'>Drowning in Data: Industry and Academic Approaches to Mixed Methods in \u201cHolistic\u201d Big Data Studies</a>",
+    33.6251241,
+    130.6180016
+  ],
+  [
+    "2014-11-02, Talk at Human Computation Conference (HCOMP), Citizen-X Workshop<br/>\n<a href='2014-11-02-hcomp-values-in-crowdsourcing'>Defining, Designing, and Evaluating Civic Values in Human Computation and Collective Action Systems (with Nathan Matias)</a>",
+    40.4416941,
+    -79.990086
+  ],
+  [
+    "2009-04-25, Talk at Media in Transition 6<br/>\n<a href='2009-04-25-mit6-media-use-wikipedia'>Evolving Governance and Media Use in Wikipedia: A Historical Account</a>",
+    42.3750997,
+    -71.1056156
+  ],
+  [
+    "2012-05-07, Panelist at Conference on Human Factors in Computing (CHI)<br/>\n<a href='2012-05-07-chi-fail-whales'>Hunting for Fail Whales: Lessons from Deviance and Failure in Social Computing</a>",
+    30.2711286,
+    -97.7436994
+  ],
+  [
+    "2016-07-16, Talk at SciPy<br/>\n<a href='2016-07-16-scipy-governing-scale'>Governing Open Source Projects at Scale: Lessons from Wikipedia&apos;s Growing Pains</a>",
+    30.2711286,
+    -97.7436994
+  ],
+  [
+    "2013-02-23, Conference proceedings talk at Conference on Computer Supported Cooperative Work<br/>\n<a href='2013-02-23-cscw-edit-sessions'>Using Edit Sessions to Measure Participation in Wikipedia (with Aaron Halfaker)</a>",
+    29.4246002,
+    -98.4951404
+  ],
+  [
+    "2016-06-09, Talk at Big Data: Critiques and Alternatives workshop<br/>\n<a href='2016-06-09-ica-successor-systems'>Successor Systems: Lessons for Big Data From Feminist Epistemology and Activism</a>",
+    33.6251241,
+    130.6180016
+  ],
+  [
+    "2013-03-01, Talk at Theorizing the Web<br/>\n<a href='2013-03-01-ttw-values-where'>Values Where? Interrogating Client-Side Scripting as a Design Process</a>",
+    40.7305991,
+    -73.9865811
+  ],
+  [
+    "2013-10-09, Talk at Annual Meeting of the Society for the Social Study of Science (4S)<br/>\n<a href='2013-10-09-ica-hadoop-grounded-theory'>Hadoop as Grounded Theory: Is an STS Approach to Big Data Possible?  the 2013 Annual Meeting of the Society for the Social Study of Science 4S</a>",
+    32.7174209,
+    -117.1627713
+  ],
+  [
+    "2011-01-03, Conference proceedings talk at Hawaii International Conference on System Sciences<br/>\n<a href='2011-01-03-hicss-trace-ethnography'>Trace Ethnography: Following Coordination through Documentary Practices</a>",
+    21.9811111,
+    -159.371111
+  ],
+  [
+    "2013-10-23, Talk at Annual Meeting of the Association of Internet Researchers (AoIR)<br/>\n<a href='2013-10-23-aoir-design-by-bot'>Design by Bot: Power and Resistance in the Development of Automated Software Agents</a>",
+    39.7391536,
+    -104.9847033
+  ],
+  [
+    "2016-06-14, Talk at Communicating with Machines workshop<br/>\n<a href='2016-06-14-ica-communicating-with-machines'>Administrative Support Bots in Wikipedia: How Automation Can Transform the Affordances of Platforms and the Governance of Communities</a>",
+    33.6251241,
+    130.6180016
+  ],
+  [
+    "2011-10-05, Conference proceedings talk at International Symposium on Wikis and Open Collaboration<br/>\n<a href='2011-10-05-wikisym-article-deletion'>Participation in Wikipedia\u2019s Article Deletion Processes (with Heather Ford)</a>",
+    37.3855745,
+    -122.0820499
+  ],
+  [
+    "2012-10-29, Panelist at International Symposium on Wikis and Open Collaboration (WikiSym 2012)<br/>\n<a href='2012-10-29-wikisym-methods'>What Aren\u2019t We Measuring?  Methods for Quantifying Wiki-Work.</a>",
+    48.3059078,
+    14.286198
+  ],
+  [
+    "2011-11-03, Talk at Annual Meeting of the Society for the Social Study of Science (4S)<br/>\n<a href='2011-11-03-4s-wikipedian-governance'>User-Generated Platforms in Wikipedian Governance</a>",
+    41.5051613,
+    -81.6934445
   ]
 ];
