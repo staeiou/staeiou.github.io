6b08ce3cda213d32149445b18107452271e78dfa
Stuart Geiger <sgeiger@gmail.com>
2017-09-26 15:42:39 -0700

    update locations

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,232 +1,342 @@
 var addressPoints = [
   [
-    "Savannah, Georgia",
+    "The Work of Sustaining Order in Wikipedia: The Banning of a Vandal, Savannah, Georgia",
     32.0835407,
     -81.0998342
   ],
   [
-    "Boston, Massachusetts",
+    "Autoethnographic Methods for Studying Data-Driven Knowledge Production, Boston, Massachusetts",
     42.3604823,
     -71.0595678
   ],
   [
-    "Austin, Texas",
+    "Black-boxing the user: internet protocol over xylophone players (IPoXP), Austin, Texas",
     30.2711286,
     -97.7436995
   ],
   [
-    "Seattle, WA",
+    "Data\u00ad-Driven Data Research Using Data and Databases: A Practical Critique of Methods and Approaches in \u201cBig Data\u201d Studies, Seattle, WA",
     47.6038321,
     -122.3300624
   ],
   [
-    "New York, NY",
+    "Algorithmic Governance: The Social Roles of Bots and Assisted Editing Tools, New York, NY",
     40.7306458,
     -73.9866136
   ],
   [
-    "Cambridge, MA",
+    "Evolving Governance and Media Use in Wikipedia: A Historical Account, Cambridge, MA",
     42.3750997,
     -71.1056157
   ],
   [
-    "Brooklyn, New York",
+    "Successor Systems: Enacting Ideological Critique Through the Development of Software, Brooklyn, New York",
     40.6501038,
     -73.9495823
   ],
   [
-    "Denver, CO",
+    "Hunting for Fail Whales: Lessons from Deviance and Failure in Social Computing, Austin, Texas",
+    30.2711286,
+    -97.7436995
+  ],
+  [
+    "The Bot Multiple: Unpacking the Materialities of Automated Software Agents, Denver, CO",
     39.7391536,
     -104.9847034
   ],
   [
-    "Berkeley, California",
+    "Demystifying Algorithmic Processes: The Case of Wikipedia, Berkeley, California",
     37.8708393,
     -122.2728639
   ],
   [
-    "Berkeley, CA",
+    "Robotic Ethics and Opportunities, Berkeley, CA",
     37.8708393,
     -122.2728639
   ],
   [
-    "Mountain View, CA",
+    "Participation in Wikipedia\u2019s Article Deletion Processes (with Heather Ford), Mountain View, CA",
     37.3893889,
     -122.0832101
   ],
   [
-    "Gdansk, Poland",
+    "Academic Researchers in Wikimedia Communities: Ethics, Methods, and Policies, Gdansk, Poland",
     54.347628,
     18.6452029
   ],
   [
-    "Hong Kong",
+    "When the Levee Breaks: Without Bots, What Happens to Wikipedia\u2019s Quality Control Processes? (with Aaron Halfaker), Hong Kong",
     22.2793278,
     114.1628131
   ],
   [
-    "Cleveland, OH",
+    "Crowdsourcing: Theoretical Considerations, Berkeley, CA",
+    37.8708393,
+    -122.2728639
+  ],
+  [
+    "User-Generated Platforms in Wikipedian Governance, Cleveland, OH",
     41.5051613,
     -81.6934446
   ],
   [
-    "Linz, Austria",
+    "What Aren\u2019t We Measuring?  Methods for Quantifying Wiki-Work., Linz, Austria",
     48.3059078,
     14.286198
   ],
   [
-    "Barcelona, Spain",
+    "\u201cThe Wisdom of Bots:\u201d An ethnographic study of the delegation of governance work to information infrastructures in Wikipedia, Barcelona, Spain",
     41.3828939,
     2.1774322
   ],
   [
-    "New York, New York",
+    "Jupyter and the Changing Rituals around Computation, New York, New York",
     40.7306458,
     -73.9866136
   ],
   [
-    "Phoenix, AZ",
+    "Bot-Based Collective Blocklists in Twitter: The Counterpublic Moderation of a Privately-Owned Networked Public Space, Phoenix, AZ",
     33.4485866,
     -112.0773456
   ],
   [
-    "Pittsburgh, PA",
+    "Big Data is Bullshit': Scoping the Next 5 Years of Digital Data Research, Seattle, WA",
+    47.6038321,
+    -122.3300624
+  ],
+  [
+    "Defining, Designing, and Evaluating Civic Values in Human Computation and Collective Action Systems (with Nathan Matias), Pittsburgh, PA",
     40.4416941,
     -79.9900861
   ],
   [
-    "Buenos Aires, Argentina",
+    "Successor Systems: The Role of Reflexive Algorithms in Enacting Ideological Critique, Buenos Aires, Argentina",
     -34.6075616,
     -58.437076
   ],
   [
-    "Newport Beach, CA",
+    "Trace Ethnography Workshop, Newport Beach, CA",
     33.6170092,
     -117.9294401
   ],
   [
-    "Davis, CA",
+    "Successor Systems: The Role of Reflexive Algorithms in Enacting Ideological Critique, Davis, CA",
     38.545379,
     -121.7445835
   ],
   [
-    "San Francisco, CA",
+    "Computational Ethnography and the Ethnography of Computation, Berkeley, California",
+    37.8708393,
+    -122.2728639
+  ],
+  [
+    "Why bots are my favorite contribution to Wikipedia, San Francisco, CA",
     37.7792808,
     -122.4192363
   ],
   [
-    "Astoria, New York",
+    "\u201cWhat the hack?\u201d Hacking culture and discourse in data science pedagogy (with Brittany Fiore-Gartland), Astoria, New York",
     40.7720145,
     -73.9302673
   ],
   [
-    "Copenhagen, Denmark",
+    "Time to Degree: Examining the Experiences of Graduate Students in the Long-Term Ecological Research Network, Copenhagen, Denmark",
     55.6867243,
     12.5700724
   ],
   [
-    "San Antonio, TX",
+    "Using Edit Sessions to Measure Participation in Wikipedia (with Aaron Halfaker), San Antonio, TX",
     29.4246002,
     -98.4951405
   ],
   [
-    "San Juan, Puerto Rico",
+    "Scraping Wikipedia Data, Berkeley, CA",
+    37.8708393,
+    -122.2728639
+  ],
+  [
+    "But it Wouldn\u2019t Be an Encyclopedia; It Would Be a Wiki: Wikipedia and the Repurposing of WikiWikiWeb, San Juan, Puerto Rico",
     18.38423905,
     -66.0534399736473
   ],
   [
-    "Tartu, Estonia",
+    "But it Wouldn\u2019t Be an Encyclopedia; It Would Be a Wiki: Wikipedia and the Repurposing of WikiWikiWeb, Tartu, Estonia",
     58.3727059,
     26.7237528
   ],
   [
-    "Albany, California",
+    "The Humanity of Artificial Intelligence, Albany, California",
     37.88687,
     -122.2977475
   ],
   [
-    "Kyoto, Japan",
+    "Governing the Commons, Berkeley, CA",
+    37.8708393,
+    -122.2728639
+  ],
+  [
+    "Trace Ethnography: An ANT Method for the Study of Sociotechnical Networks, New York, NY",
+    40.7306458,
+    -73.9866136
+  ],
+  [
+    "Improving Wikipedia\u2019s Notifications to Rejected Contributors, Kyoto, Japan",
     35.0185804,
     135.763835
   ],
   [
-    "Amsterdam, the Netherlands",
+    "Bot Politics: How is Automation Changing the Wikipedian Society?  Critical Point of View II, Amsterdam, the Netherlands",
     52.3745403,
     4.89797550561798
   ],
   [
-    "Bangkok, Thailand",
+    "Size Matters: How Big Data Changes Everything, Bangkok, Thailand",
     13.7542529,
     100.493087
   ],
   [
-    "Bangalore, India",
+    "The Wisdom of Bots: A Critique of \u2018Self-Organization\u2019 in Wikipedia, Bangalore, India",
     12.9791198,
     77.5912997
   ],
   [
-    "Dublin, Ireland",
+    "Defense Mechanism or Socialization Tactic? Improving Wikipedia\u2019s Notifications to Rejected Contributors, Dublin, Ireland",
     53.3497645,
     -6.2602732
   ],
   [
-    "Vancouver, BC",
+    "Community, Impact, and Credit: Where Do I Submit My Papers?, San Antonio, TX",
+    29.4246002,
+    -98.4951405
+  ],
+  [
+    "Supporting Change from Outside Systems with Design and Data, Cambridge, MA",
+    42.3750997,
+    -71.1056157
+  ],
+  [
+    "Situated knowledges and successor systems: developing CSCW systems to enact ideological critiques, Vancouver, BC",
     49.262428,
     -123.11554
   ],
   [
-    "Alexandria, Egypt",
+    "Conceptions and Misconceptions Academics Hold About Wikipedia, Alexandria, Egypt",
     31.199004,
     29.894378
   ],
   [
-    "Lihue, Hawaii",
+    "Peer Production and Wikipedia, Berkeley, CA",
+    37.8708393,
+    -122.2728639
+  ],
+  [
+    "Trace Ethnography: Following Coordination through Documentary Practices, Lihue, Hawaii",
     21.981111,
     -159.371111
   ],
   [
-    "Long Beach, CA",
+    "Machine-Generated Content: Bots and the Governance of Wikipedia, Long Beach, CA",
     33.78538945,
     -118.158049315311
   ],
   [
-    "San Diego, CA",
+    "Design by Bot: Power and Resistance in the Development of Automated Software Agents, Denver, CO",
+    39.7391536,
+    -104.9847034
+  ],
+  [
+    "Community Sustainability in Wikipedia: A Review of Research and Initiatives, San Francisco, CA",
+    37.7792808,
+    -122.4192363
+  ],
+  [
+    "Hadoop as Grounded Theory: Is an STS Approach to Big Data Possible?  the 2013 Annual Meeting of the Society for the Social Study of Science 4S, San Diego, CA",
     32.7174209,
     -117.1627714
   ],
   [
-    "Fukuoka, Japan",
+    "\u2019The Internet is Here\u2019: The Virtuality of \u2018On-line Communities in Physical Spaces, Cleveland, OH",
+    41.5051613,
+    -81.6934446
+  ],
+  [
+    "Drowning in Data: Industry and Academic Approaches to Mixed Methods in \u201cHolistic\u201d Big Data Studies, Fukuoka, Japan",
     33.6251241,
     130.6180016
   ],
   [
-    "Orlando, Florida",
+    "Governing Open Source Projects at Scale: Lessons from Wikipedia's Growing Pains, Austin, Texas",
+    30.2711286,
+    -97.7436995
+  ],
+  [
+    "The Social Roles of Bots and Assisted Editing Tools, Orlando, Florida",
     28.5423999,
     -81.3794368
   ],
   [
-    "Daegu, South Korea",
+    "Actor-Network Theory, Berkeley, CA",
+    37.8708393,
+    -122.2728639
+  ],
+  [
+    "Moderating Online Conversation Spaces, Berkeley, CA",
+    37.8708393,
+    -122.2728639
+  ],
+  [
+    "Administrative Support Bots in Wikipedia: How Automation Can Transform the Affordances of Platforms and the Governance of Communities, Fukuoka, Japan",
+    33.6251241,
+    130.6180016
+  ],
+  [
+    "Successor Systems: Lessons for Big Data From Feminist Epistemology and Activism, Fukuoka, Japan",
+    33.6251241,
+    130.6180016
+  ],
+  [
+    "Successor Systems: The Role of Reflexive Algorithms in Enacting Ideological Critique, Daegu, South Korea",
     35.8494916,
     128.5599518
   ],
   [
-    "Evanston, IL",
+    "Trace literacy: a framework for holistically conceptualizing newcomer socialization in socio-technical systems, Evanston, IL",
     42.0447388,
     -87.6930459
   ],
   [
-    "Washington, DC",
+    "Values Where? Interrogating Client-Side Scripting as a Design Process, New York, NY",
+    40.7306458,
+    -73.9866136
+  ],
+  [
+    "Working With/in Wikipedia: Infrastructures of Knowing and Knowledge Production, Washington, DC",
     38.8949549,
     -77.0366456
   ],
   [
-    "Arlington, Virginia",
+    "Algorithms as agents of gatekeeping, governance, and articulation work in Wikipedia, Fukuoka, Japan",
+    33.6251241,
+    130.6180016
+  ],
+  [
+    "Moderating harassment in Twitter with blockbots: a counterpublic and algorithmic strategy, Astoria, New York",
+    40.7720145,
+    -73.9302673
+  ],
+  [
+    "Where Are the Missing Wikipedians? The Sociology of a Bot, Arlington, Virginia",
     38.8903961,
     -77.0841585
   ],
   [
-    "College Station, TX",
+    "A Communicative Ethnography of Argumentative Strategies in a Wikipedian Content Dispute, College Station, TX",
     30.6253463,
     -96.3271538
+  ],
+  [
+    "Does Facebook Have Civil Servants? On Governmentality and Computational Social Science, Vancouver, BC",
+    49.262428,
+    -123.11554
   ]
 ];
